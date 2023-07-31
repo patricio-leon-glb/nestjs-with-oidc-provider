fe5e4b160beb29c4b6706b0860b31d3de57f74ec
Patricio León <patricio.leon@globant.com>
2023-07-31 18:08:32 -0400

    feat(oauth2-module): added logger support

--- src/oauth2-client/oauth2-client.controller.ts
+++ src/oauth2-client/oauth2-client.controller.ts
@@ -1,13 +1,16 @@
-import { Controller, Get, Req } from '@nestjs/common';
+import { Controller, Get, Logger, Req } from '@nestjs/common';
 
 @Controller({
   path: '/client',
   version: '1',
 })
 export class Oauth2ClientController {
+  constructor(private readonly logger: Logger) {}
+
   @Get('/callback')
   public async callBack(@Req() req): Promise<any> {
-    const { query } = req;
-    return { query };
+    const { params, query, body, headers, originalUrl } = req;
+    this.logger.log({ path: originalUrl, params, query, body });
+    return { callback: { params, query, body, headers, originalUrl } };
   }
 }
--- src/oauth2-client/oauth2-client.module.ts
+++ src/oauth2-client/oauth2-client.module.ts
@@ -1,6 +1,7 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import { Oauth2ClientController } from './oauth2-client.controller';
 @Module({
   controllers: [Oauth2ClientController],
+  providers: [Logger],
 })
 export class OAuth2ClientModule {}
